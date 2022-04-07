c51fa0fb044e73eb24b009bf858663c8d36010c7
Hugo Flávio <hugoflavio@live.com>
2022-04-07 18:25:11 -0300

    Programa 10

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -1,9 +1,17 @@
-const a = 54;
-const b = 45;
-const c = 86;
+const custo = 100;
+const valorDeRevenda = 150;
 
-if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
-    console.log("true")
-}else {
-    console.log("false")
+const imposto = 20; // Referente a porcentagem > 20%
+
+const custoTotal = custo + ((custo * imposto)/100);
+const custoTotalFixed = custoTotal.toFixed(2); // Valor arredondado com duas casas decimais
+
+const lucro = valorDeRevenda - custoTotalFixed;
+
+const lucroLoteMilhar = lucro * 1000;
+
+if (custo <= 0 || valorDeRevenda <= 0){
+    console.log("Erro, insirra um valor maior que 0");   
+} else {
+    console.log(lucroLoteMilhar);
 }
