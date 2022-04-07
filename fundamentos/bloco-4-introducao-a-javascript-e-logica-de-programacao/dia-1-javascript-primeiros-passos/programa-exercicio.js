f0df0440200867c5cbc207e86acab3ca93f5839f
Hugo Flávio <hugoflavio@live.com>
2022-04-07 14:24:29 -0300

    Programa 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -1,10 +1,11 @@
 const a = 100;
-const b = 100;
+const b = 400;
+const c = 300;
 
-if (a > b) {
+if (a > b && a > c) {
     console.log("O primeiro número é o maior");
-} else if (a == b) {
-    console.log("Os números são iguais");
-} else {
+} else if (b > c) {
     console.log("O segundo número é o maior");
+} else {
+    console.log("O terceiro número é o maior");
 }
