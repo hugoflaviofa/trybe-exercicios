2c72c1d1d502fab1979bc78ab33bc51a64efbb4e
Hugo Flávio <hugoflavio@live.com>
2022-04-06 21:21:11 -0300

    Programa 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -0,0 +1,9 @@
+const a = 100;
+const b = 2;
+
+const soma = a + b;
+const subtracao = a - b;
+const multiplicacao = a / b;
+const modulo = a % b;
+
+console.log(soma, subtracao, multiplicacao, modulo)
