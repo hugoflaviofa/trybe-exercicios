a41e72577111e11e740ed0acf42e5ca571399148
Hugo Flávio <hugoflavio@live.com>
2022-04-07 17:41:27 -0300

    Programa 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -1,17 +1,9 @@
-const nota = 60;
+const a = 55;
+const b = 44;
+const c = 85;
 
-if (nota >= 90 && nota <=100){
-    console.log("A");
-}else if (nota >= 80 && nota < 90){
-    console.log("B");
-}else if (nota >= 70 && nota < 80){
-    console.log("C");
-}else if (nota >= 60 && nota < 70){
-    console.log("D");
-}else if (nota >= 50 && nota < 60) {
-    console.log("E");
-}else if (nota < 50 && nota >= 0){
-    console.log("F");
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+    console.log("true")
 }else {
-    console.log("erro");
+    console.log("false")
 }
