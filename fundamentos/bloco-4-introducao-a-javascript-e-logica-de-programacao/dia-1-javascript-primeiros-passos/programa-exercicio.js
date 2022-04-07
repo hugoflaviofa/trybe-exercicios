2632aecfa71d56281d4f2735dbc18ab9924bb347
Hugo Flávio <hugoflavio@live.com>
2022-04-07 16:48:17 -0300

    Programa 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -1,16 +1,17 @@
-const pecaDeXadrez = "TOrrE";
+const nota = 60;
 
-switch(pecaDeXadrez.toLowerCase()) {
-    case "rei": console.log("Movimento em qualquer direção, porém apenas uma casa por vez");
-        break;
-    case "Rainha": console.log("Movimento em qualquer direção, quantas casas quiser");
-        break;
-    case "torre": console.log("Movimento em linha reta, quantas casas quiser");
-        break;
-    case "bispo": console.log("Movimento na diagonal, quantas casas quiser");
-        break;
-    case "cavalo": console.log("Movimento em L");
-        break;
-    case "peão": console.log("Movimento para frente, uma casa por vez");
-        break;
+if (nota >= 90 && nota <=100){
+    console.log("A");
+}else if (nota >= 80 && nota < 90){
+    console.log("B");
+}else if (nota >= 70 && nota < 80){
+    console.log("C");
+}else if (nota >= 60 && nota < 70){
+    console.log("D");
+}else if (nota >= 50 && nota < 60) {
+    console.log("E");
+}else if (nota < 50 && nota >= 0){
+    console.log("F");
+}else {
+    console.log("erro");
 }
