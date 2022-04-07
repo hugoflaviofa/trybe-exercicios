b207f3c16fc7df54f0c89e48fd8df25c57a381d2
Hugo Flávio <hugoflavio@live.com>
2022-04-07 20:31:28 -0300

    Programa 11

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa-exercicio.js
@@ -1,17 +1,41 @@
-const custo = 100;
-const valorDeRevenda = 150;
+const salarioBruto = 3000;
 
-const imposto = 20; // Referente a porcentagem > 20%
+const inss1 = 1556.94;
+const inss2 = 2594.92;
+const inss3 = 5189.82;
+const inssMax = 570.88;
 
-const custoTotal = custo + ((custo * imposto)/100);
-const custoTotalFixed = custoTotal.toFixed(2); // Valor arredondado com duas casas decimais
+let salarioDescontInss = 0;
 
-const lucro = valorDeRevenda - custoTotalFixed;
+const ir1 = 1909.98;
+const ir2 = 2826.65;
+const ir3 = 3751.05;
+const ir4 = 4664.68;
 
-const lucroLoteMilhar = lucro * 1000;
+let salarioLiquido = 0;
 
-if (custo <= 0 || valorDeRevenda <= 0){
-    console.log("Erro, insirra um valor maior que 0");   
-} else {
-    console.log(lucroLoteMilhar);
+if (salarioBruto <= inss1){
+    salarioDescontInss = salarioBruto - ((salarioBruto * 8) / 100);
+} else if (salarioBruto > inss1 && salarioBruto <= inss2) {   
+    salarioDescontInss = salarioBruto - ((salarioBruto * 9) / 100); 
+}else if (salarioBruto > inss2 && salarioBruto <= inss3) {  
+    salarioDescontInss = salarioBruto - ((salarioBruto * 11) / 100); 
+}else if (salarioBruto > inss3) {   
+    salarioDescontInss = salarioBruto - inssMax; 
 }
+
+// console.log(salarioDescontInss.toFixed(2));
+
+if  (salarioDescontInss <= ir1){
+    salarioLiquido = salarioDescontInss;
+}else if (salarioDescontInss > ir1 && salarioDescontInss <= ir2){
+    salarioLiquido = salarioDescontInss - (((salarioDescontInss *  7.5) / 100) - 142.8);
+}else if (salarioDescontInss > ir2 && salarioDescontInss <= ir3){
+    salarioLiquido = salarioDescontInss - (((salarioDescontInss *  15) / 100) - 354.8);
+}else if (salarioDescontInss > ir3 && salarioDescontInss <= ir4){
+    salarioLiquido = salarioDescontInss - (((salarioDescontInss *  22.5) / 100) - 636.13);
+}else if (salarioDescontInss > ir4){
+    salarioLiquido = salarioDescontInss - (((salarioDescontInss *  27.5) / 100) - 869.36);
+}
+
+console.log(`Seu salário Bruto é R$${salarioBruto} e o liquido é R$${salarioLiquido.toFixed(2)}`);
