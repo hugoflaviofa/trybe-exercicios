004a440cbec7bec95df12f0f68419f273e190446
Hugo Flávio <hugoflavio@live.com>
2022-04-11 16:09:46 -0300

    JavaScript - Array e loop For - Resolução exercicio 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
@@ -0,0 +1,9 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let list = [];
+
+for(let index = 1; index <= 25; index += 1){
+    list.push(index / 2);
+}
+
+console.log(list);
