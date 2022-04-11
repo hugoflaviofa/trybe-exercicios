e0caaf6e7cb3c29e1e53811de6a2d58d0b11fff4
Hugo Flávio <hugoflavio@live.com>
2022-04-11 15:57:59 -0300

    JavaScript - Array e loop For - Resolução exercicio 6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio6.js
@@ -0,0 +1,14 @@
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let countOddNumbers = 0;
+
+for(number of numbers){
+    
+    if(number % 2 === 1){
+        countOddNumbers += 1;
+    }
+}
+
+console.log(countOddNumbers);
