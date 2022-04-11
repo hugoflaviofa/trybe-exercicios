7a7de5ab38d0182ae8051f887202d8b2fe28691e
Hugo Flávio <hugoflavio@live.com>
2022-04-11 16:07:35 -0300

    JavaScript - Array e loop For - Resolução exercicio 8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,9 @@
+//Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let list = [];
+
+for(let index = 1; index <= 25; index += 1){
+    list.push(index);
+}
+
+console.log(list);
