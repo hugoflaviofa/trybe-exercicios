60f483bd6facb49bc5f6952e52ef8f145286d2fe
Hugo Flávio <hugoflavio@live.com>
2022-04-17 18:46:31 -0300

    Exercicios de 1 a 5 - Parte II

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_de_6_a_8_parte_II.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_de_6_a_8_parte_II.js
@@ -19,7 +19,7 @@ function verificaPalindrome(word){
 
 //2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-function maxArray(array){
+function maxIndexInArray(array){
 
     let maxNumber = Math.max(...array);
     let index = array.findIndex(array => array === maxNumber)
@@ -29,7 +29,7 @@ function maxArray(array){
 
 let arraytest = [-2, 3, 6, 7, 10, 1];
 
-// console.log(maxArray(arraytest));
+// console.log(maxIndexInArray(arraytest));
 
 //3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
@@ -43,4 +43,61 @@ function minArray(array){
 
 let arraytest2 = [2, 4, 6, 7, 10, 0, -3];
 
-console.log(minArray(arraytest2));
+// console.log(minArray(arraytest2));
+
+//4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function biggerNameInArray(arrayNames){
+
+    let listSizeNames = [];
+
+    for(index of arrayNames){
+        let numberLetters = index;
+        listSizeNames.push(numberLetters.length)
+    }
+
+    let indexTheBiggerName = maxIndexInArray(listSizeNames);
+    let theBiggerName = arrayNames[indexTheBiggerName];
+
+    return theBiggerName;
+
+}
+
+let arraytest3 = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+// console.log(biggerNameInArray(arraytest3));
+
+//5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function repeatedNumber(arrayNumber){
+
+    //Funcionamento: Armazenar dentro de um novo Array por quantas vezes o determidado valor em um indice se repete, comparando o mesmo apenas com os números que vem a seguir. O primeiro número que mais se repetir terá o maior valor armazenado no seu indice.
+    //Ex: [2, 3, 2, 5, 8, 2, 3] - O número "2" no indice 0 se repete 3 vezes quando comparado com os nímeros seguintes; O número "3" no indice 1 se repete 2 vezes; Número "2" no indice 2 se repete 2 vezes; [...].
+    //No novo array fica armazenado as repeticoes de cada indice do primeiro array, logo o indice com maior valor no segundo array é respectivo ao numero que mais se repete do primeiro array. Assim, descubro qual o maior valor do segundo array e retorno o mesmo indice, porem, referente ao primeiro array. 
+
+    let count = 1;
+
+    let arrayCount = [];
+
+    for(let index = 0; index < arrayNumber.length; index += 1){
+        for(let index2 = index + 1; index2 < arrayNumber.length; index2 += 1){
+
+            if(arrayNumber[index] === arrayNumber[index2]){
+                count += 1;
+            }
+        }
+
+        arrayCount.push(count);
+        count = 1;
+    }
+
+    let indexNumber = maxIndexInArray(arrayCount);
+    let numberRepeted = arrayNumber[indexNumber];
+
+    return numberRepeted;
+}
+
+
+let arraytest4 = [2, 3, 2, 5, 8, 2, 3];
+
+// console.log(repeatedNumber(arraytest4));
