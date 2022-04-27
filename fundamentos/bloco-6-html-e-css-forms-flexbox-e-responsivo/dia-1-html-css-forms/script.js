4e1bb3a942da6090e323b039acbed92f1f6de090
Hugo Flávio <hugoflavio@live.com>
2022-04-27 20:01:03 -0300

    Exercicio 6.1 - Criando formulario

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+    let submitButton = document.getElementById('submit');
+    let resetButton = document.getElementById('reset');
+    // let nameInput = document.getElementById('name-input');
+
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault();
+    });
+
+    resetButton.addEventListener('click', function(){
+
+        document.getElementById('name-input').innerText = '';
+        document.getElementById('email-input').innerText = '';
+        document.getElementById('text-area').innerText = '';
+        document.getElementById('name-input').focus();
+    });
+}
