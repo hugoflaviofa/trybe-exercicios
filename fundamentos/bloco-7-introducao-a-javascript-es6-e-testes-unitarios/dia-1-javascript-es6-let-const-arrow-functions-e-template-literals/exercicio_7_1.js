bee3e4e12a6afed03f284f1d0449c3b8a4a5af85
Hugo Flávio <hugoflavio@live.com>
2022-05-10 14:32:27 -0300

    Exercicios 7.1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio_7_1.js
@@ -0,0 +1,62 @@
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// - Modifique a estrutura da função para que ela seja uma arrow function.
+// - Modifique as concatenações para template literals.
+// - Copie o código abaixo.
+
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+// testingScope(true);
+
+const testingScopeArowFunc = (escopo) => {
+  let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+  if (escopo === true) {
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(`${ifScope} o que estou fazendo aqui ? :O`);
+}
+
+// testingScopeArowFunc(false);
+
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o métodoarray.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2, 22, 31, 18, 14];
+
+const arrayNumberSort = (array) => {
+
+  array.sort();
+  let saveArray = []; 
+  // console.log(array);
+  for (i = 0; i < array.length; i += 1) {
+
+    if (array[i] > 9) {
+
+      let save = array.shift();
+
+      array.push(save);
+      
+    }
+    
+  }
+
+  return array;
+  
+}
+
+console.log(`Os números ${arrayNumberSort(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha
