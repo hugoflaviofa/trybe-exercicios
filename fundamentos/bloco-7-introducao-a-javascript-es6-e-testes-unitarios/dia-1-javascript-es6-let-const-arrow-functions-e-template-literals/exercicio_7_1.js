0d0171317298385e74e342b19daa707674e5844f
Hugo Flávio <hugoflavio@live.com>
2022-05-11 13:45:48 -0300

    Exercicios 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio_7_1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio_7_1.js
@@ -31,7 +31,7 @@ const testingScopeArowFunc = (escopo) => {
 }
 
 // testingScopeArowFunc(false);
-
+//---------------------------------------------------------------------------------------------------------------
 // Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 // Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
 // Bônus (opcional): tente fazer o mesmo exercício utilizando o métodoarray.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
@@ -40,6 +40,58 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2, 22, 31, 18, 14];
 
 const arrayNumberSort = array => array.sort(function(a, b){return a - b});
 const arrayNumberSorT = array => array.sort(func = (a, b) => b - a);
+const arrayNumberSoRT = array => array.sort((a, b) => b - a);
   
-console.log(`Os números ${arrayNumberSort(oddsAndEvens)} se encontram ordenados de forma crescente!`); 
-console.log(`Os números ${arrayNumberSorT(oddsAndEvens)} se encontram ordenados de forma crescente!`); 
+// console.log(`Os números ${arrayNumberSort(oddsAndEvens)} se encontram ordenados de forma crescente!`); 
+// console.log(`Os números ${arrayNumberSorT(oddsAndEvens)} se encontram ordenados de forma crescente!`); 
+//---------------------------------------------------------------------------------------------------------------
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+const fatNumber = number => {
+  let fatResult = number;
+  for(i = number - 1; i > 0; i -= 1) {
+    fatResult *= i;
+  }
+  return fatResult;
+}
+
+// const fatNumber2 = number => (i = 0) > 0 ? console.log('primeiro') : console.log('segundo');
+const fatNumber2 = number => number > 0 ? number * fatNumber2(number - 1) : 1; //A função chama a si mesmo e aguarda o resultado de cada chamanda até number < 0, retornando 1 e realizando a multiplicação inversa da ordem de cada chamada.
+
+// number/4 > 0 (true) => return 4 * func(number/4 - 1)?          == 4 * 6 === 24
+//    number/3 > 0 (true) => return 3 * func(number/3 - 1)?           == 3 * 2 === 6
+//        number/2 > 0 (true) => return 2 * func(number/2 - 1)?        == 2 * 1 === 2 
+//            number/1 > 0 (true) => return 1 * func(number/1 - 1)?      == 1 * 1 === 1 
+//                number/0 > 0 (false) => return 1!
+
+// console.log(fatNumber2(4));
+//---------------------------------------------------------------------------------------------------------------
+//Crie uma função que receba uma frase e retorne qual a maior palavra.
+// Exemplo:
+//      longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+const frase = 'Antônio foi no banheiro e não sabemos o que aconteceu';
+
+const bigWord = (word) => {
+
+  const arrayWords = word.split(' ');
+  let arrayLength = [];
+
+  for(i of arrayWords){
+    arrayLength.push(i.length);
+  }
+  
+  const bigNumber = Math.max(...arrayLength);
+
+  const bigIndex = arrayLength.findIndex((param) => param === bigNumber);
+  // const bigIndex = arrayLength.findIndex(
+  //   function(param) {
+  //     return param === bigNumber;
+  //   }
+  // );
+  return console.log(arrayWords[bigIndex]);
+}
+
+// bigWord(frase);
